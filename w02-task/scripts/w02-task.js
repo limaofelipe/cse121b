331e972741feeb7353365233d5e4147f2fcb917b
lima.felipe <63979005+limaofelipe@users.noreply.github.com>
2024-01-21 22:22:47 -0300

    Add files via upload

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,37 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = "Felipe Lima"
+const year = new Date()
+let currentYear = year.getFullYear()
+let profilePicture = "./images/placeholder.png"
+
+
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById("name")
+const foodElement = document.getElementById("food")
+const yearElement = document.querySelector("#year")
+const imageElement = document.querySelector('img')
+
+/* Step 4 - Adding Content */
+imageElement.setAttribute("src", `${profilePicture}`)
+nameElement.innerHTML = (`<strong> ${fullName}<strong>`)
+yearElement.innerHTML = (`${currentYear}`)
+imageElement.setAttribute("alt", `profile image of ${fullName}`)
+
+
+/* Step 5 - Array */
+let foods = ["Pizza" , "Hamburguer", "Fried Chicken", "French Fries"]
+
+foodElement.innerHTML += `<br>${foods}`
+
+
+
+
+
+
+
